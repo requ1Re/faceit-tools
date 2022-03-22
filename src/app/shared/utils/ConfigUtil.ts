98c6010d6d206c8a66d7a18f91ced0bd5fac225f
require7 <patrick.list@pm.me>
2022-03-22 13:40:39 +0100

    add blog link

--- src/app/shared/utils/ConfigUtil.ts
+++ src/app/shared/utils/ConfigUtil.ts
@@ -1,4 +1,4 @@
-import { faAddressBook, faMapMarkedAlt, faUser, IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { faAddressBook, faBlog, faEnvelopeOpenText, faList, faMapMarkedAlt, faParagraph, faTextHeight, faUser, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 export namespace Config {
   export interface AppConfig {
@@ -9,7 +9,8 @@ export namespace Config {
     id: number;
     name: string;
     route: string;
-    icon: IconDefinition
+    icon: IconDefinition,
+    external: boolean
   };
 }
 
@@ -17,9 +18,10 @@ export class ConfigUtil {
 
   static readonly CONFIG: Config.AppConfig = {
     tools: [
-      { id: 0, name: 'Map Picker', route: '/tools/picker', icon: faMapMarkedAlt },
-      { id: 1, name: 'Stats', route: '/tools/stats', icon: faUser },
-      { id: 2, name: 'Account Finder', route: '/tools/finder', icon: faAddressBook }
+      { id: 0, name: 'Map Picker', route: '/tools/picker', icon: faMapMarkedAlt, external: false },
+      { id: 1, name: 'Stats', route: '/tools/stats', icon: faUser, external: false },
+      { id: 2, name: 'Account Finder', route: '/tools/finder', icon: faAddressBook, external: false },
+      { id: 3, name: 'Tutorials & Changelogs', route: 'https://blog.faceit-tools.app/', icon: faBlog, external: true }
     ]
   }
 
