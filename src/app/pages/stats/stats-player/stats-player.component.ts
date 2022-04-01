6d16470dcc44cb81b10563e11ced9d7ba11179b8
require7 <patrick.list@pm.me>
2022-04-01 14:07:25 +0200

    update store implementation for stats-player component

--- src/app/pages/stats/stats-player/stats-player.component.ts
+++ src/app/pages/stats/stats-player/stats-player.component.ts
@@ -1,23 +1,18 @@
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { faSteam } from '@fortawesome/free-brands-svg-icons';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { Actions } from '@ngrx/effects';
-import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { combineLatest, first } from 'rxjs';
 import { BaseComponentWithStatsStore } from 'src/app/shared/components/base-stats-store/base-stats-store';
 import { FaceIT } from 'src/app/shared/models/FaceIT';
-import { ApiService } from 'src/app/shared/services/api.service';
 import { ErrorService } from 'src/app/shared/services/error.service';
 import {
   loadPlayerOverviewByNickname,
   loadPlayerStatsByID,
 } from 'src/app/shared/store/stats/stats.actions';
 import { StatsState } from 'src/app/shared/store/stats/stats.reducer';
-import {
-  getPlayerOverviews,
-  getPlayerStats,
-} from 'src/app/shared/store/stats/stats.selector';
 import { EloUtil } from 'src/app/shared/utils/EloUtil';
 
 @Component({
@@ -29,18 +24,19 @@ export class StatsPlayerComponent extends BaseComponentWithStatsStore {
 
   faSteam = faSteam;
 
+  playerOverviews: FaceIT.PlayerOverview.Player[] = [];
+  playerStats: FaceIT.Player.PlayerStats[] = [];
+
   playerName = '';
   playerId = '';
 
-  playerOverviewData: FaceIT.PlayerOverview.Player | undefined;
-  playerStatsData: FaceIT.Player.PlayerStats | undefined;
+  playerOverviewData: FaceIT.PlayerOverview.Player;
+  playerStatsData: FaceIT.Player.PlayerStats;
 
   error = false;
 
   constructor(
-    private router: Router,
     private route: ActivatedRoute,
-    private api: ApiService,
     private errorService: ErrorService,
     store: Store<StatsState>,
     actions$: Actions
@@ -50,10 +46,30 @@ export class StatsPlayerComponent extends BaseComponentWithStatsStore {
 
   init() {
     this.registerSubscription(
-      this.route.paramMap.subscribe((params) => {
-        this.playerName = params.get('playerName') ?? '';
-        this.loadData();
-      })
+      combineLatest([
+        this.playerOverviews$,
+        this.playerStats$,
+        this.route.paramMap,
+      ])
+        .pipe(first())
+        .subscribe((data) => {
+          console.log('[DEBUG] Got combined data, calling loadData', data);
+
+          this.playerOverviews = data[0];
+          this.playerStats = data[1];
+          this.playerName = data[2].get('playerName') ?? '';
+
+          this.loadData();
+
+          const findOverview = this.playerOverviews.find(
+            (playerOverview) => playerOverview.nickname === this.playerName
+          );
+          if (!findOverview) {
+            this.store.dispatch(
+              loadPlayerOverviewByNickname({ nickname: this.playerName })
+            );
+          }
+        })
     );
 
     this.registerSubscription(
@@ -61,40 +77,38 @@ export class StatsPlayerComponent extends BaseComponentWithStatsStore {
         this.error = errorObj.error;
       })
     );
-
-    this.registerSubscription(
-      this.playerOverviews$.subscribe((data) => {
-        this.playerOverviewData = data.find(
-          (playerOverview) => playerOverview.nickname === this.playerName
-        );
-        if (this.playerOverviewData) {
-          this.playerId = this.playerOverviewData.player_id;
-
-          this.hasPlayerStatsInStore(this.playerId).subscribe((hasInStore) => {
-            if (!hasInStore) {
-              this.store.dispatch(loadPlayerStatsByID({ id: this.playerId }));
-            }
-          });
-        }
-      })
-    );
-    this.registerSubscription(
-      this.playerStats$.subscribe((data) => {
-        this.playerStatsData = data.find(
-          (playerStatsData) => playerStatsData.player_id === this.playerId
-        );
-      })
-    );
   }
 
   loadData() {
-    this.hasPlayerOverviewInStore(this.playerName).subscribe((hasInStore) => {
-      if (!hasInStore) {
-        this.store.dispatch(
-          loadPlayerOverviewByNickname({ nickname: this.playerName })
+    this.registerSubscription(
+      this.playerOverviews$.subscribe((playerOverviews) => {
+        const findOverview = playerOverviews.find(
+          (playerOverview) => playerOverview.nickname === this.playerName
         );
-      }
-    });
+        if (findOverview) {
+          this.playerOverviewData = findOverview;
+          this.playerId = findOverview.player_id;
+
+          const findStats = this.playerStats.find(
+            (playerStats) => playerStats.player_id === this.playerId
+          );
+          if (!findStats) {
+            this.store.dispatch(loadPlayerStatsByID({ id: this.playerId }));
+          }
+        }
+      })
+    );
+
+    this.registerSubscription(
+      this.playerStats$.subscribe((playerStats) => {
+        const findStats = playerStats.find(
+          (playerStats) => playerStats.player_id === this.playerId
+        );
+        if (findStats) {
+          this.playerStatsData = findStats;
+        }
+      })
+    );
   }
 
   getRecentResults(data: FaceIT.Player.PlayerStats) {
