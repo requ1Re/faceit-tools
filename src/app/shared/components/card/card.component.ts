30ce47f34499ec152dee3b72a474f2a5934b43f7
require7 <patrick.list@pm.me>
2022-05-29 01:52:37 +0200

    theme update

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+
+  @Input()
+  noPadding = false;
+
+  @Input('customClass')
+  klass: string;
 
   constructor() { }
 
