c66aa83cf0e22d8ad2c021e10f85ced2a2f4e1d1
Patrick List <requ1re@pm.me>
2022-08-24 09:57:14 +0200

    remove blog link from navbar for now

--- src/app/shared/utils/ConfigUtil.ts
+++ src/app/shared/utils/ConfigUtil.ts
@@ -1,4 +1,4 @@
-import { faAddressBook, faBlog, faEnvelopeOpenText, faList, faMapMarkedAlt, faParagraph, faTextHeight, faUser, IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { faAddressBook, faMapMarkedAlt, faUser, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 export namespace Config {
   export interface AppConfig {
@@ -21,7 +21,7 @@ export class ConfigUtil {
       { id: 0, name: 'Map Picker', route: '/picker', icon: faMapMarkedAlt, external: false },
       { id: 1, name: 'Stats', route: '/stats', icon: faUser, external: false },
       { id: 2, name: 'Account Finder', route: '/finder', icon: faAddressBook, external: false },
-      { id: 3, name: 'Tutorials & Changelogs', route: 'https://blog.faceit-tools.app/', icon: faBlog, external: true }
+      // { id: 3, name: 'Tutorials & Changelogs', route: 'https://blog.faceit-tools.app/', icon: faBlog, external: true }
     ]
   }
 
