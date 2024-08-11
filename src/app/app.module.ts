693f73deb8ca8c22d7e67312bedb3276c51051d4
Patrick List <patrick.list@pm.me>
2024-08-11 19:07:21 +0200

    update angular to v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { EffectsModule } from '@ngrx/effects';
@@ -19,31 +19,25 @@ import { SharedModule } from './shared/shared.module';
 import { AppStoreModule } from './shared/store/app-store.module';
 import { SpriteWrapperComponent } from './sprite-wrapper/sprite-wrapper.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ToolsOverviewComponent,
-    NotFoundComponent,
-    SpriteWrapperComponent,
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    HttpClientModule,
-    AppRoutingModule,
-    SharedModule,
-    FontAwesomeModule,
-    AppStoreModule,
-    StoreModule.forRoot({}),
-    EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument(),
-    BrowserAnimationsModule
-  ],
-  providers: [
-    LogService,
-    ErrorService,
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: FaceITAPIInterceptor, multi: true }
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        ToolsOverviewComponent,
+        NotFoundComponent,
+        SpriteWrapperComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        AppRoutingModule,
+        SharedModule,
+        FontAwesomeModule,
+        AppStoreModule,
+        StoreModule.forRoot({}),
+        EffectsModule.forRoot([]),
+        StoreDevtoolsModule.instrument(),
+        BrowserAnimationsModule], providers: [
+        LogService,
+        ErrorService,
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: FaceITAPIInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {}
