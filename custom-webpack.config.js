7895186d934eafd316d9676e19c130440acfa18c
Patrick List <patrick.list@pm.me>
2024-08-12 14:35:58 +0200

    cleanup

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,15 +1,9 @@
 const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
-const { EnvironmentPlugin } = require('webpack');
-
-require('dotenv').config();
 
 module.exports = {
   plugins: [
     new MomentLocalesPlugin({
       localesToKeep: ["de", "en", "ru"],
-    }),
-    new EnvironmentPlugin([
-      'FACEIT_API_KEY'
-    ])
+    })
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
    production: false,
-   API_URL: "undefined",
+   API_URL: "",
    FACEIT_API_KEY: "1e75b804-0829-4b11-93bd-e5898696b00a"
 };
