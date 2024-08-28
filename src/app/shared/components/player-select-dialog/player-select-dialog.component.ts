5a26463ccd269eb8ddc5d35341604f4db714fa37
Patrick List <patrick.list@pm.me>
2024-08-28 23:34:19 +0200

    fix player select dialog and add i18n

--- src/app/shared/components/player-select-dialog/player-select-dialog.component.ts
+++ src/app/shared/components/player-select-dialog/player-select-dialog.component.ts
@@ -1,31 +1,25 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { TranslateModule } from '@ngx-translate/core';
 import { FaceIT } from '../../models/FaceIT';
 import { ApiService } from '../../services/api.service';
 import { BaseComponent } from '../base/base';
 import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-
 
 @Component({
-    selector: 'app-player-select-dialog',
-    templateUrl: './player-select-dialog.component.html',
-    styleUrls: ['./player-select-dialog.component.scss'],
-    standalone: true,
-    imports: [
-    FaIconComponent,
-    LoadingSpinnerComponent
-],
+  selector: 'app-player-select-dialog',
+  templateUrl: './player-select-dialog.component.html',
+  styleUrls: ['./player-select-dialog.component.scss'],
+  standalone: true,
+  imports: [FaIconComponent, LoadingSpinnerComponent, TranslateModule],
 })
-export class PlayerSelectDialogComponent
-  extends BaseComponent
-  implements OnInit
-{
+export class PlayerSelectDialogComponent extends BaseComponent implements OnInit {
   constructor(
     private apiService: ApiService,
     public dialogRef: MatDialogRef<PlayerSelectDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: PlayerSelectDialogData
+    @Inject(MAT_DIALOG_DATA) public data: PlayerSelectDialogData,
   ) {
     super();
   }
@@ -39,7 +33,7 @@ export class PlayerSelectDialogComponent
   players: FaceIT.Search.Item[] = [];
 
   ngOnInit(): void {
-    if(this.data.instantSearch){
+    if (this.data.instantSearch) {
       this._value = this.data.value;
       this.submit();
     }
@@ -54,31 +48,29 @@ export class PlayerSelectDialogComponent
     if (!this.disableSubmit) {
       this.disableSubmit = true;
       this.registerSubscription(
-        this.apiService
-          .searchFACEITAccountsByString(this._value)
-          .subscribe((data) => {
-            if (data) {
-              this.players = data.items;
-            }else{
-              this.isErroneous = true;
-              this.errorText = "Search has failed";
-            }
-            this.disableSubmit = false;
-          })
+        this.apiService.searchFACEITAccountsByString(this._value).subscribe((data) => {
+          if (data) {
+            this.players = data.items;
+          } else {
+            this.isErroneous = true;
+            this.errorText = 'Search has failed';
+          }
+          this.disableSubmit = false;
+        }),
       );
     }
   }
 
-  getSkillLevel(player: FaceIT.Search.Item){
-    return player.games.find((g) => g.name === "cs2")?.skill_level ?? 1;
+  getSkillLevel(player: FaceIT.Search.Item) {
+    return player.games.find((g) => g.name === 'cs2')?.skill_level ?? 1;
   }
 
-  selectPlayer(player: FaceIT.Search.Item){
+  selectPlayer(player: FaceIT.Search.Item) {
     this.dialogRef.close(player);
   }
 }
 
 export interface PlayerSelectDialogData {
-  value: string,
-  instantSearch: boolean
+  value: string;
+  instantSearch: boolean;
 }
